956f5498cbaf69d96c3bc54a36a80ba6cf331988
Ashleigh McMahon <ashleighmc@me.com>
2023-10-03 09:07:10 -0600

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// code your solution here
+
+
+
+function razzle (lawyer = "Billy", target = "'em"){
+    console.log (`${lawyer} razzle-dazzles ${target}!`)
+}
+razzle()
+razzle ("Methusaleh", "T'Challah")
+
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`)
+}
+console.log (saturdayFun())
+
+function mondayWork(toDo="go to the office"){
+    return (`This Monday, I will ${toDo}.`);
+}
+
+function demoChain(name) {
+    const part1 = "hi";
+    return function () {
+      const part2 = "there";
+      return function () {
+        console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+      };
+    };
+  }
+  
+  demoChain("Dr. Stephen Strange")()();
+
+  function wrapAdjective(name) {
+    const first = "special";
+    return function (adjective) {
+      return `You are ${name}${adjective}${name}!`
+      const second = "a hard worker";
+      return `You are *${second}*!`;
+      const third = "a dedicated programmer";
+      return 'You are ||${third}||!'
+      
+    };
+  }
+  
+  console.log(wrapAdjective("Ashleigh")(third));
+
+
